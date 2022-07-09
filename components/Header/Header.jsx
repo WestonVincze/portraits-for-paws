02bae23a93ec385ae06f044ad698ac21b6d30456
Weston Vincze <westonvincze@gmail.com>
2022-07-09 00:33:16 -0400

    initial commit remainder

---
+++ components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Head from "next/head";
+
+function Header() {
+  return(
+    <>
+      <Head>
+        <title>Portraits For Paws</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>
+        Title
+      </h1>
+    </>
+  )
+}
+
+export default Header;
